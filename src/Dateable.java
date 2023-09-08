7f9dcb051395f8bf2ea83f05912f42f5a85a8185
howarddeng <howardd2026@student.cis.edu.hk>
2023-09-08 17:37:45 +0800

    complete

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        System.out.println("How old are you?");
+        Scanner scanner = new Scanner(System.in);
+        int userAge = scanner.nextInt();
+        int dateAge = (userAge / 2) + 7;
+        System.out.println(userAge+"-year olds should read "+(100-userAge)+" pages before giving up on a book");
     }
 }
